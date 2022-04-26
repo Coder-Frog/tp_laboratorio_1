485e726b3bc47e0edcfa3e51348608120a4b39af
Coder-Frog <j.e.delmar.87@gmail.com>
2022-04-26 09:55:09 -0300

    ASD

---
+++ TP_[2]/src/functions.h
@@ -0,0 +1,37 @@
+#ifndef FUNCTIONS_H_
+#define FUNCTIONS_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "functions.h"
+#include "arrayPassenger.h"
+
+void menu();
+
+int dataInt(int base, int top);
+
+float dataFloat(int base, int top);
+
+void stringEntry(int option,int top, char *stringChain);
+
+void registerPassenger();
+
+void afterCheckIn();
+
+void pressKey();
+
+void modifyPassenger();
+
+void modifyName(int id);
+
+void modifyLastName(int id);
+
+void modifyPrice(int id);
+
+void modifyFlyCode(int id);
+
+void modifyTypePassenger(int id);
+
+
+#endif
